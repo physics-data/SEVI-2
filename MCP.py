6239590af30dd1efcf450a2afaeab8c18655e9ec
YiyangWu <xgreenlandforwyy@gmail.com>
2021-09-09 23:26:39 +0800

    finish MCP Simulation

--- MCP.py
+++ MCP.py
@@ -1,8 +1,17 @@
+import argparse
+import sys
+psr = argparse.ArgumentParser()
+psr.add_argument('ipt', help='input')
+psr.add_argument('opt', help='output')
+psr.add_argument('-t', '--with-truth', dest='withtruth', action='store_true')
+psr.add_argument('-b', '--break-at', dest='stop', type=int, default=-1)
+args = psr.parse_args()
+
 import h5py
 import numpy as np
 import pandas as pd
 
-with h5py.File("test.h5", "r") as ipt:
+with h5py.File(args.ipt, "r") as ipt:
     DetectedElectrons = ipt["DetectedElectrons"][()]
 
 # DetectedElectrons = DetectedElectrons[DetectedElectrons["ImageId"] == 0]
@@ -64,15 +73,23 @@ def GenerateOneImage(x0s, z0s, A, σ, ρ) :
     return theImage
 
 
+Nimages_in = DetectedElectrons["ImageId"].max() + 1
+Nimage_out = Nimages_in if args.stop == -1 else min(args.stop, Nimages_in)
 Image_dt = np.dtype([("ImageId", np.int32), ("Image", np.uint8, (size, size))])
-flush_num = 20
+flush_num = 100
 ImageGroup = eleDF.groupby("ImageId")
-with h5py.File("testMCP.h5", "w") as opt :
-    opt.create_dataset("FinalImage", shape=33, dtype=Image_dt)
+with h5py.File(args.opt, "w") as opt :
+    opt.create_dataset("FinalImage", shape=(Nimage_out,), dtype=Image_dt)
+    if args.withtruth :
+        with h5py.File(args.ipt, "r") as ipt :
+            for keyname in list(ipt.keys()) :
+                opt.create_dataset(keyname, data=ipt[keyname])
+
     counter = 0
     batch_counter = 0
     FinalImages = np.empty(flush_num, dtype=Image_dt)
     for ImageId, ThisImageElec in ImageGroup :
+        if batch_counter * flush_num + counter == args.stop : break
         if counter == flush_num :
             opt["FinalImage"][batch_counter * flush_num:batch_counter * flush_num + counter] = FinalImages  # flush data to disk
             counter = 0
@@ -81,5 +98,4 @@ with h5py.File("testMCP.h5", "w") as opt :
         print("ImageId={0}, counter={1}, batch_counter={2}".format(ImageId, counter, batch_counter))
         FinalImages["ImageId"][counter] = ImageId
         counter += 1
-        if batch_counter == 1 and counter == 13 : break
     opt["FinalImage"][batch_counter * flush_num:batch_counter * flush_num + counter] = FinalImages[:counter]  # flush data to disk
